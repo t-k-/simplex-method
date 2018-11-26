be2e37117d9a4cb0a4bdf59a67d7ce181be220f1
t-k- <clock126@126.com>
2018-11-25 19:51:52 -0500

    simplify tableau code if ensure b_bar >= 0

--- pseudo.py
+++ pseudo.py
@@ -1,57 +1,38 @@
 def simplex_tableau(names, c, A_bar, b_bar):
-    # get dimensions
     m, n = A_bar.shape
-    # check if it has redundant rows
+    # check inputs
     if m != matrix_rank(A_bar):
-        print('not full rank matrix')
-        quit()
+        raise Exception("not full rank matrix")
+    if (b_bar < 0).any():
+        raise Exception("initial b_bar not all >= 0")
     # initialize xi and z_0
     xi = -c
     z_0 = np.matrix([0.0])
-    print_tableau(names, xi, z_0, A_bar, b_bar)
-    # start iterations
     for iteration in range(99):
         # update objective row (zero those on eye matrix)
         A_bar_N = A_bar[:,m:n]
-        xi_B = xi[0:m]
-        xi_N = xi[m:n]
+        xi_B, xi_N = xi[0:m], xi[m:n]
         xi_N_update = xi_N.T - xi_B.T * A_bar_N
         xi = horz_cat([pad_zeros(m), xi_N_update]).T
         # update objective row (update z_0)
         z_0 -= xi_B.T * b_bar
         # new iteration ready ...
-        print('iteration', iteration)
-        print_tableau(names, xi, z_0, A_bar, b_bar)
-        if (xi <= 0).all() and (b_bar >= 0).all():
-            print('optimal!')
-            break
+        if (xi <= 0).all():
+            return # optimal
         # get entering position
-        if (xi > 0).any(): # find k where xi_k first > 0
-            k = (xi > 0).argmax() # Bland's rule
-        else: # just choose the first in this case
-            k = m + 0
-        print('k = %u (entering)' % k)
+        k = (xi > 0).argmax() # Bland's rule
         A_bar_k = A_bar[:, k]
         # lower bounded?
-        if (A_bar_k <= 0).all() and (b_bar >= 0).all():
-            print('no lower bound!')
-            quit()
+        if (A_bar_k <= 0).all():
+            raise Exception("no lower bound")
         # get exiting position
-        np.seterr(divide='ignore') # ignore divided-by-zero error
         tmp = b_bar / A_bar_k
-        if (b_bar >= 0).all():
-            unselect = (A_bar_k <= 0)
-            tmp[unselect] = float("inf")
+        tmp[A_bar_k <= 0] = float("inf")
         r = min_idx(tmp) # Bland's rule
-        theta = amin(tmp)
-        print('r = %u (exiting), theta = %f' % (r, theta))
-        print('pivoting...')
         # swap k, r columns in A_bar, xi and names 
         A_bar[:, [k,r]] = A_bar[:, [r,k]]
         xi[[k,r]] = xi[[r,k]]
         names[[k,r]] = names[[r,k]]
         # update A_bar and b_bar using row operations
-        B = A_bar[:, 0:m]
-        A_bar = inv(B) * A_bar
-        b_bar = inv(B) * b_bar
-        print_tableau(names, xi, z_0, A_bar, b_bar)
+        B = A_bar[:, 0:m] # must be linear independent, i.e. basis
+        A_bar, b_bar = inv(B) * A_bar, inv(B) * b_bar
--- simplex.py
+++ simplex.py
@@ -9,6 +9,8 @@ from numpy import argmin as min_idx
 from numpy import amin as amin
 from numpy import zeros
 
+np.seterr(divide='ignore') # ignore divided-by-zero error
+
 def print_float(x):
     return str("%8.3f" % x)
 
@@ -47,6 +49,10 @@ def simplex_tableau(names, c, A_bar, b_bar):
     if m != matrix_rank(A_bar):
         print('not full rank matrix')
         quit()
+    # check if initial b_bar is BFS
+    if (b_bar < 0).any():
+        print('initial b_bar not all >= 0')
+        quit()
     # initialize xi and z_0
     xi = -c
     z_0 = np.matrix([0.0])
@@ -55,47 +61,37 @@ def simplex_tableau(names, c, A_bar, b_bar):
     for iteration in range(99):
         # update objective row (zero those on eye matrix)
         A_bar_N = A_bar[:,m:n]
-        xi_B = xi[0:m]
-        xi_N = xi[m:n]
+        xi_B, xi_N = xi[0:m], xi[m:n]
         xi_N_update = xi_N.T - xi_B.T * A_bar_N
         xi = horz_cat([pad_zeros(m), xi_N_update]).T
         # update objective row (update z_0)
         z_0 -= xi_B.T * b_bar
         # new iteration ready ...
-        print('## Iteration', iteration)
+        print('\n', '## Iteration', iteration)
         print_tableau(names, xi, z_0, A_bar, b_bar)
-        if (xi <= 0).all() and (b_bar >= 0).all():
+        if (xi <= 0).all():
             print('optimal!')
             break
         # get entering position
-        if (xi > 0).any(): # find k where xi_k first > 0
-            k = (xi > 0).argmax() # Bland's rule
-        else: # just choose the first in this case
-            k = m + 0
-        print('k = %u (entering)' % k)
+        k = (xi > 0).argmax() # Bland's rule
+        print('k = %u (entering)' % k, end=", ")
         A_bar_k = A_bar[:, k]
         # lower bounded?
-        if (A_bar_k <= 0).all() and (b_bar >= 0).all():
+        if (A_bar_k <= 0).all():
             print('no lower bound!')
             quit()
         # get exiting position
-        np.seterr(divide='ignore') # ignore divided-by-zero error
         tmp = b_bar / A_bar_k
-        if (b_bar >= 0).all():
-            unselect = (A_bar_k <= 0)
-            tmp[unselect] = float("inf")
+        tmp[A_bar_k <= 0] = float("inf")
         r = min_idx(tmp) # Bland's rule
-        theta = amin(tmp)
-        print('r = %u (exiting), theta = %f' % (r, theta))
-        print('pivoting...')
+        print('r = %u (exiting), theta = %f' % (r, amin(tmp)))
         # swap k, r columns in A_bar, xi and names 
         A_bar[:, [k,r]] = A_bar[:, [r,k]]
         xi[[k,r]] = xi[[r,k]]
         names[[k,r]] = names[[r,k]]
         # update A_bar and b_bar using row operations
-        B = A_bar[:, 0:m]
-        A_bar = inv(B) * A_bar
-        b_bar = inv(B) * b_bar
+        B = A_bar[:, 0:m] # must be linear independent, i.e. basis
+        A_bar, b_bar = inv(B) * A_bar, inv(B) * b_bar
         print_tableau(names, xi, z_0, A_bar, b_bar)
 
 # theory-style calculation
@@ -105,6 +101,10 @@ def simplex_theory(names, c, A_bar, b_bar):
     # check if it has redundant rows
     if m != matrix_rank(A_bar):
         print('not full rank matrix')
+        quit()
+    # check if initial b_bar is BFS
+    if (b_bar < 0).any():
+        print('initial b_bar not all >= 0')
         quit()
     # initialize xi and z_0
     xi = -c
@@ -176,12 +176,13 @@ def main():
     # b_bar = np.matrix([ 2.0, 1.0]).T
 
     # example input #3
-	# http://zhangxiaoyang.me/categories/intro-to-algorithms-tutorial/intro-to-algorithms-tutorial-6-1.html
-    names = np.array( ['s1', 's2', 'x1', 'x2'])
-    c     = np.matrix([ 0.0, 0.0,  6.8,  2.7]).T
-    A_bar = np.matrix([[1.0, 0.0, -668.0, -883.0],
-                       [0.0, 1.0, -1.5, -1.2]])
-    b_bar = np.matrix([ -16800.0, -525.0]).T
+    # http://zhangxiaoyang.me/categories/intro-to-algorithms-tutorial/intro-to-algorithms-tutorial-6-1.html
+    names = np.array( ['a1', 'a2',  'x1',   'x2', 's1', 's2'])
+    M = 99999.0
+    c     = np.matrix([   M,   M,    6.8,    2.7,  0.0,  0.0]).T
+    A_bar = np.matrix([[1.0, 0.0,  668.0,  883.0, -1.0,  0.0],
+                       [0.0, 1.0,    1.5,    1.2,  0.0, -1.0]])
+    b_bar = np.matrix([ 16800.0, 525.0]).T
 
     # example input #4
     # https://www.hrwhisper.me/introduction-to-simplex-algorithm/
